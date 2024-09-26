a51478427a81e64262b70608fb23d123cb45717f
ASHWANI KUMAR <85477554+ashwanil23@users.noreply.github.com>
2024-09-26 17:45:08 +0530

    26-09-2024

---
+++ src/com/dsa/lecture/code/stack/ArrayImplementation.java
@@ -0,0 +1,75 @@
+package com.dsa.lecture.code.stack;
+
+import java.util.NoSuchElementException;
+
+public class ArrayImplementation {
+    private int top;
+    private int[] stackArray;
+
+    public ArrayImplementation(int capacity){
+        this.top = -1;
+        this.stackArray = new int[capacity];
+    }
+
+    public int size(){
+        return top+1;
+    }
+    public boolean isFull(){
+        return stackArray.length == size();
+    }
+    public boolean isEmpty(){
+        return top < 0;
+    }
+    public void push(int data){
+        if(isFull()){
+            throw new NoSuchElementException("Stack empty");
+        }else{
+            this.top++;
+            this.stackArray[top] = data;
+        }
+    }
+    public int pop(){
+        if(isEmpty()){
+            throw new NoSuchElementException("Stack empty");
+        }else{
+            int element = this.stackArray[top];
+            top--;
+            return element;
+        }
+    }
+
+    public int peek(){
+        if (isEmpty()){
+            throw new NoSuchElementException("Stack empty");
+        }else{
+            return this.stackArray[top];
+        }
+    }
+
+    public void display(){
+        if (isEmpty()) throw new NoSuchElementException("Stack empty");
+        else {
+            int tempTop = top;
+            while(tempTop >= 0){
+                System.out.print(this.stackArray[tempTop]+" ");
+                tempTop--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayImplementation stack = new ArrayImplementation(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(10);
+        stack.display();
+        System.out.println();
+        System.out.print("Top->"+stack.peek());
+        stack.pop();
+        System.out.println();
+        stack.display();
+        System.out.println();
+        System.out.print("Top->"+stack.peek());
+    }
+}
---
+++ src/com/dsa/lecture/code/stack/ListImplementation.java
@@ -0,0 +1,90 @@
+package com.dsa.lecture.code.stack;
+
+import java.util.NoSuchElementException;
+
+public class ListImplementation {
+    private ListNode top;
+    private int length;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public ListImplementation() {
+        this.top = null;
+        this.length = 0;
+    }
+
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    public void push(int data){
+        if (isEmpty()){
+            this.top = new ListNode(data);
+            this.length++;
+            return;
+        }else{
+            ListNode newNode = new ListNode(data);
+            newNode.next = this.top;
+            this.top = newNode;
+            this.length++;
+        }
+    }
+
+    public int pop(){
+        if (isEmpty()){
+            throw new NoSuchElementException("Stack is empty");
+        }else{
+            ListNode temp = top;
+            this.top = this.top.next;
+            length--;
+            return temp.data;
+        }
+    }
+
+    public void display(){
+        if(isEmpty()){
+            throw new NoSuchElementException("Stack is empty");
+        }else{
+            ListNode temp = this.top;
+            while(temp != null){
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+    public int peek(){
+        if(isEmpty()){
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return this.top.data;
+    }
+
+    public static void main(String[] args) {
+        ListImplementation stack = new ListImplementation();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        System.out.print("Top -> "+stack.peek());
+        System.out.println();
+
+        stack.display();
+
+        stack.pop();
+
+        System.out.println();
+        System.out.print("Top -> "+stack.peek());
+
+        System.out.println();
+        stack.display();
+    }
+}
